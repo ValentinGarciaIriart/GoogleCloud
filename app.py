eec569da1870c3e148f3ba5d38ea247e36909f7e
ValentinGarciaIriart <valentingarciairiart@gmail.com>
2024-06-08 14:20:30 -0300

    commit final

--- app.py
+++ app.py
@@ -15,21 +15,17 @@ def resultado():
 
         if operacion == 'sumar':
             resultado = num1 + num2
-            resultado = "Maquinola"
         elif operacion == 'restar':
             resultado = num1 - num2
-            resultado = "Maquinola"
         elif operacion == 'multiplicar':
             resultado = num1 * num2
-            resultado = "Maquinola"
         elif operacion == 'dividir':
             if num2 != 0:
                 resultado = num1 / num2 
-                resultado = "Maquinola"
             else:
-                resultado = 'Error: No se puede dividir por cero (boludoooo)'
+                resultado = 'Error: No se puede dividir por cero'
         else:
-            resultado = 'Operación no válida (o sea man jijo)'
+            resultado = 'Operación no válida (jijo)'
 
         return render_template('resultado.html', resultado=resultado)
 
